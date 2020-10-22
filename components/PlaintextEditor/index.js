983db121327844fd469b8f0662ff92942ad315a0
Ben Orgera <benorg@me.com>
2020-10-21 22:38:09 -0400

    Added state for file as string to plaintext editor, enabled plaintext editor, added textarea for editing

--- components/PlaintextEditor/index.js
+++ components/PlaintextEditor/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import css from './style.css';
+import path from 'path';
 
 function PlaintextEditor({ file, write }) {
   console.log(file, write);
+
+  const [fileString, setFileString] = useState('Loading...');
+
+  useEffect(() => {
+  	(async () => {
+  	    const text = await file.text();
+  	    console.log(text);
+  	    setFileString(text);
+  	})();
+  }, [file]); // whenever file changes internally, update the UI
+
   return (
     <div className={css.editor}>
-      <h3>TODO</h3>
-      <i>text/plain</i>
+      <h3>{path.basename(file.name)}</h3>
+      <textarea value={fileString}></textarea>
     </div>
   );
 }
